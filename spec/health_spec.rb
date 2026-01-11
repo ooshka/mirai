6f216f16b4ba81e2ed4644166ef67fd2cb7c303a
Alex Wadey <wadeyalex@gmail.com>
2026-01-11 14:44:00 -0800

    Add initial ruby test files and get a practice run of the server going

---
+++ spec/health_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.describe "Health" do
+  it "returns ok" do
+    get "/health"
+    expect(last_response_status).to eq(200)
+    expect(JSON.parse(last_response.body).to eq({ "ok" => true})
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+ENV["RACK_ENV"] = "test"
+
+require "rack/test"
+require_relative "../app"
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app = App
+end
+
+RSpec.configure do |config|
+  config.include RSpecMixin
+end
