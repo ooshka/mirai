cb6c987587798d6499443771d2cd706285063018
Alex Wadey <wadeyalex@gmail.com>
2026-01-11 16:20:36 -0800

    fix rspec tests

--- spec/health_spec.rb
+++ spec/health_spec.rb
@@ -3,7 +3,7 @@
 RSpec.describe "Health" do
   it "returns ok" do
     get "/health"
-    expect(last_response_status).to eq(200)
-    expect(JSON.parse(last_response.body).to eq({ "ok" => true})
+    expect(last_response.status).to eq(200)
+    expect(JSON.parse(last_response.body)).to eq({ "ok" => true})
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@
 ENV["RACK_ENV"] = "test"
 
 require "rack/test"
+require "json"
 require_relative "../app"
 
 module RSpecMixin
